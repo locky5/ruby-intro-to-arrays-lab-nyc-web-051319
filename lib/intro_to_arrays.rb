8557a9b3f5fcd52fd4e832afce1242228fd79f85
Kevin Dai <daikevin19@yahoo.com>
2019-04-29 05:07:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,7 +21,9 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.first
+  return array.first
 end
 
-def 
+def last_element_with_array_methods(array)
+  return array.last
+end
